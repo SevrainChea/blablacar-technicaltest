54fb83fccbe85138be35c60994a9c6c71f1f1631
SevrainChea <sevrain.chea@gmail.com>
2020-10-04 18:39:54 +0200

    Add TestFileRunner class to run testFiles

---
+++ src/main/java/com/chea/blablacar/technicaltest/exceptions/TestFileRunnerException.java
@@ -0,0 +1,8 @@
+package com.chea.blablacar.technicaltest.exceptions;
+
+public class TestFileRunnerException extends RuntimeException {
+
+    public TestFileRunnerException(String message, Throwable t) {
+        super(message, t);
+    }
+}
